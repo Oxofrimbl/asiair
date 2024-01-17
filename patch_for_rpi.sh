62c61e2198b1ae9b9bb948972a87be6579125cae
Oxofrimbl <156840153+Oxofrimbl@users.noreply.github.com>
2024-01-17 21:05:46 +0000

    Create patch_for_rpi.sh

---
+++ patch_for_rpi.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Change return code of a function, Ghidra is your friend here if you want to figure out details
+binary_patch=$(cat <<EOF
+001f4a10: 01  .
+001f4a13: e3  .
+001f4a60: 01  .
+001f4a63: e3  .
+EOF
+)
+
+fake_licence=$(cat <<EOF
+{"sn":"00000000","auth_code":"0000000000000000000000.00000000","digest":"0000000000000000000000000000000000000000000=","sign":"0"}
+EOF
+)
+
+fake_cpuinfo=
+
+md5=($(md5sum $1))
+tempfile=$(mktemp -t tmp.XXXXXXXXXX)
+timestamp=$(date +%s)
+
+echo $binary_patch > $tempfile
+
+if [[ $md5 != "ba4e082adb279175f33fe61ed46e4cd3" ]]; then
+        echo "File verification failed, just works with 2.1 (?)"
+        exit -1
+fi
+
+#Creating Fake CPUID file
+sed -e "s/.*Serial.*/Serial          : 0000000000000000/" /proc/cpuinfo > /boot/fake_cpuinfo
+
+#TODO this step needs to be persistet in rc.local
+sudo mount --bind /boot/fake_cpuinfo /proc/cpuinfo
+
+
+echo "Creating backup of tiles to modify"
+cp -v $1 $1.bak.$timestamp
+cp -v /boot/zwoair_license /boot/zwoair_license.bak.$timestamp
+cp -v /home/pi/.ZWO/zwoair_license /home/pi/.ZWO/zwoair_license.bak.$timestamp
+
+#echo "Creating fake license file"
+echo $fake_license > /boot/zwoair_license
+echo $fake_license > /home/pi/.ZWO/zwoair_license
+
+#echo "Patching binary"
+xxd -c1 -r $tempfile $1
+
+#Creating Fake CPUID file
+sed -e "s/.*Serial.*/Serial          : 0000000000000000/" /proc/cpuinfo > /boot/fake_cpuinfo
+echo "Patching should have been successeded, howeer persist this line in rc.local before asiair gets started"
+#TODO make this automatic
+sudo mount --bind /boot/fake_cpuinfo /proc/cpuinfo
+echo "sudo mount --bind /boot/fake_cpuinfo /proc/cpuinfo"
+
+#Cleanup
+rm $tempfile
